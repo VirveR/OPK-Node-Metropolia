e353811f56894e73b863667eb3f0057455e63c60
VirveR <112824021+VirveR@users.noreply.github.com>
2023-04-29 21:31:50 +0300

    luku3 valmis

---
+++ luku3/index.js
@@ -0,0 +1,21 @@
+// Node.js @Metropolia AMK, luku 3
+// Uuden palvelimen luonti
+// Virve Rajasärkkä 2023
+
+//ottaa käyttöön Node.js:n http-moduulin
+const http = require('http');
+const port = 3000;
+
+//palauttaa serveri-olion, joka käsittelee yllä määrätyn portin kautta
+//sille tulevat pyynnöt (req/uest) ja vastaukset niihin (res/ponse)
+const server = http.createServer((req, res) => {
+    res.statuscode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`Server is running at port ${port}.`);
+});
+
+//jei, toimii!
