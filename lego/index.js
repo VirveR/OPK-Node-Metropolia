86a7d311ec6f3f049f62a1aaf115fdaf152797d8
VirveR <112824021+VirveR@users.noreply.github.com>
2023-05-01 23:26:26 +0300

    Luvusta 9 lähdetty side questille

--- lego/index.js
+++ lego/index.js
@@ -2,21 +2,29 @@
 // Luku 6, Tietokannan käyttö Node.js-sovelluksissa
 // Luku 8, Testaus
 // Luku 9, Suojaus
+// Luku 10, Mallimoottorit
 // Palvelimen aloitustiedosto
 // Virve Rajasärkkä 2023
 
 // supersalainen sala-avain löytyy tästä
 process.env.SECRET_KEY = "5b1a3923cc1e1e19523fd5c3f20b409509d3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84d";
 
+const {join} = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const query = require('./db/osat');
 const auth = require('./services/authenticate');
 const app = express();
+app.use(express.static(join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 const port = 3000;
 
 // reitit REST-päätepisteisiin osat-moduulin kautta
+app.get('/legopaivakirja', (req, res) => {
+    res.render('legopaivakirja');
+});
 app.post('/kirjaudu', auth.kirjaa);
 app.get('/api/osat', auth.todenna, query.haeOsat);
 app.get('/api/osat/:osanro', auth.todenna, query.haeOsanro);
